ad22cd5407c89a248b735de162c22455e43a6419
awhamil <andrew.hamilton@gmail.com>
2023-04-17 13:37:02 -0400

    change mrz.py to take input image as command line param

--- mrz.py
+++ mrz.py
@@ -1,14 +1,15 @@
 from hubconf import custom
 from PIL import Image
-import pathlib
-
-IMAGE_PATH = 'data/test/images'
+import sys
 
 model = custom(path_or_model='yolov7-tiny-mrz.pt') 
 
-dir = pathlib.Path(IMAGE_PATH)
-images = []
-for path in dir.glob('*.[jp][pn]g'):
-  images.append(Image.open(str(path)))
-df = model(images).pandas().xywh
-print(df)
+img = Image.open(sys.argv[1])
+img_preds = model([img]).xyxy[0]
+best_pred = img_preds[0].tolist()
+pred_dict = {
+    'xyxy': best_pred[0:4],
+    'conf': best_pred[4]
+}
+
+print(pred_dict)
